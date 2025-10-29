dcb1d1681b51bd2678ef263d8d05a3a95d5254dd
RoshanNaidu <101449801+RoshanNaidu@users.noreply.github.com>
2025-10-28 23:35:12 -0400

    Refactor fit and predict methods in apputil.py

--- apputil.py
+++ apputil.py
@@ -8,31 +8,21 @@ class GroupEstimate(object):
         self.group_values = None
     
     def fit(self, X, y):
-        # Combine X and y into a single DataFrame
-        data = pd.DataFrame(X).copy()
-        data['target'] = y
-        
-        # Calculate the group estimate based on the specified method
-        if self.estimate == "mean":
-            self.group_estimates = data.groupby(self.estimate)['target'].mean().to_dict()
-        elif self.estimate == "median":
-            self.group_estimates = data.groupby(self.estimate)['target'].median().to_dict()
+        df = pd.concat([X, y.rename('y')], axis=1)
+        if self.estimate == 'mean':
+            self.group_values = df.groupby(list(X.columns))['y'].mean()
         else:
-            raise ValueError("Estimate must be either 'mean' or 'median'")
-        
-        return self
+            self.group_values = df.groupby(list(X.columns))['y'].median()
 
-    def predict(self, X):
-        # Predict the estimate based on the group estimates
-        predictions = []
-        missing_count = 0
-        for value in X[self.estimate]:
-            if value in self.group_estimates:
-                predictions.append(self.group_estimates[value])
+    def predict(self, X_list):
+        preds, missing = [], 0
+        for row in X_list:                      
+            key = tuple(row)
+            if key in self.group_values.index:
+                preds.append(self.group_values.loc[key])
             else:
-                predictions.append(float('nan'))  # or some default value
-                missing_count += 1
-        if missing_count > 0:
-            print(f"Warning: {missing_count} missing groups were found during prediction.")
-        return predictions
-    
+                preds.append(np.nan)
+                missing += 1
+        if missing:
+            print(f"{missing} group(s) missing from training data.")
+        return np.array(preds)
