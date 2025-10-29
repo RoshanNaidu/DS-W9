9923dff4ea2995ee60a75811b4214540d8142250
RoshanNaidu <101449801+RoshanNaidu@users.noreply.github.com>
2025-10-28 23:37:59 -0400

    Fix numpy import and return type in GroupEstimate
    Added missing import for numpy and ensured output is a numpy array.

--- apputil.py
+++ apputil.py
@@ -1,4 +1,35 @@
+# import pandas as pd
+
+# class GroupEstimate(object):
+#     def __init__(self, estimate='median'):
+#         if estimate not in ['mean', 'median']:
+#             raise ValueError("estimate must be 'mean' or 'median'")
+#         self.estimate = estimate
+#         self.group_values = None
+    
+#     def fit(self, X, y):
+#         df = pd.concat([X, y.rename('y')], axis=1)
+#         if self.estimate == 'mean':
+#             self.group_values = df.groupby(list(X.columns))['y'].mean()
+#         else:
+#             self.group_values = df.groupby(list(X.columns))['y'].median()
+
+#     def predict(self, X_list):
+#         preds, missing = [], 0
+#         for row in X_list:                      
+#             key = tuple(row)
+#             if key in self.group_values.index:
+#                 preds.append(self.group_values.loc[key])
+#             else:
+#                 preds.append(np.nan)
+#                 missing += 1
+#         if missing:
+#             print(f"{missing} group(s) missing from training data.")
+#         return np.array(preds)
+
+
 import pandas as pd
+import numpy as np
 
 class GroupEstimate(object):
     def __init__(self, estimate='median'):
@@ -25,4 +56,4 @@ class GroupEstimate(object):
                 missing += 1
         if missing:
             print(f"{missing} group(s) missing from training data.")
-        return np.array(preds)
+        return np.array(preds)  # Ensure output is a numpy array
